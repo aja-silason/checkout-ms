59d7c4d3a5e7dad3c91bbd8f5fa6ac5f6d03aa97
aja-silason <ananiasjaimeaugusto@gmail.com>
2025-04-03 12:30:49 +0100

    feat: add module

--- src/checkout/checkout.module.ts
+++ src/checkout/checkout.module.ts
@@ -1,8 +1,13 @@
 import { Module } from '@nestjs/common';
 import { CheckoutService } from './checkout.service';
 import { CheckoutController } from './checkout.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Checkout } from './entities/checkout.entity';
 
 @Module({
+  imports: [
+    TypeOrmModule.forFeature([Checkout])
+  ],
   controllers: [CheckoutController],
   providers: [CheckoutService],
 })
