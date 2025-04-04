8fae3dd237f926295936f7af58826ba3c0203ee9
aja-silason <ananiasjaimeaugusto@gmail.com>
2025-04-04 10:25:31 +0100

    feat: add this thing

--- src/checkout/dto/create-checkout.dto.ts
+++ src/checkout/dto/create-checkout.dto.ts
@@ -2,6 +2,7 @@ import { Type } from "class-transformer";
 import { ArrayNotEmpty, IsArray, IsInt, isNotEmpty, IsNotEmpty, IsNumber, IsPositive, ValidateNested } from "class-validator";
 
 export class CheckoutItemDto {
+
     @IsInt()
     @IsNotEmpty()
     @IsNumber()
@@ -13,12 +14,15 @@ export class CheckoutItemDto {
     @IsNumber()
     @IsPositive()
     product_id: number;
+
 }
 
 export class CreateCheckoutDto {
+
     @IsArray()
     @ArrayNotEmpty()
     @ValidateNested({each: true})
     @Type(() => CheckoutItemDto)
     items: CheckoutItemDto[];
+    
 }
