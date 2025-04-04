8893e10e350d5ac22686790b334ee1f1a3d5edbe
aja-silason <ananiasjaimeaugusto@gmail.com>
2025-04-04 10:01:52 +0100

    feat: send project

--- src/checkout/dto/create-checkout.dto.ts
+++ src/checkout/dto/create-checkout.dto.ts
@@ -1,8 +1,24 @@
+import { Type } from "class-transformer";
+import { ArrayNotEmpty, IsArray, IsInt, isNotEmpty, IsNotEmpty, IsNumber, IsPositive, ValidateNested } from "class-validator";
+
 export class CheckoutItemDto {
+    @IsInt()
+    @IsNotEmpty()
+    @IsNumber()
+    @IsPositive()
     quantity: number;
+    
+    @IsInt()
+    @IsNotEmpty()
+    @IsNumber()
+    @IsPositive()
     product_id: number;
 }
 
 export class CreateCheckoutDto {
+    @IsArray()
+    @ArrayNotEmpty()
+    @ValidateNested({each: true})
+    @Type(() => CheckoutItemDto)
     items: CheckoutItemDto[];
 }
